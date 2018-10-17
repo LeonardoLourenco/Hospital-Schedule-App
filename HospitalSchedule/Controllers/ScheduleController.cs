98c1b5aeffcdbe4e307a4c81ff67d3a0a4861fff
LeonardoLourenço <leonardo-lourenco@hotmail.com>
2018-10-17 16:00:22 +0100

    Created a controller and a view

---
+++ HospitalSchedule/Controllers/ScheduleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalSchedule.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public IActionResult ScheduleCreation()
+        {
+            return View();
+        }
+    }
+}
