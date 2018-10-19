f7785d695ee2a92b44a3f5b8166b897b5aeeae18
LucianoBrido <lucianofernandes@live.com.pt>
2018-10-19 10:48:29 +0100

    19/10  Modelo e view dos enfermeiros criados

--- HospitalSchedule/Models/EnfermeiroClass.cs
+++ HospitalSchedule/Models/EnfermeiroClass.cs
@@ -8,16 +8,28 @@ namespace HospitalSchedule.Models
 {
     public class EnfermeiroClass
     {
-        [Required]
+       
         public int EnfermeiroID { get; set; }
         [Required]
         public string nome { get; set; }
 
         public string Bloco { get; set; }
+
         [Required]//1->60 anos;2-filhos menores;3-outros
         public int tipo { get; set; }
 
-        
+        [Required]
+        public string especialidades { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+
 
     }
 }
