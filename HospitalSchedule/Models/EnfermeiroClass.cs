0ef523ba598e6515f88c142b817d090257e91030
LucianoBrido <lucianofernandes@live.com.pt>
2018-10-19 10:18:40 +0100

    Modelo Enfermeiro criado

---
+++ HospitalSchedule/Models/EnfermeiroClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalSchedule.Models
+{
+    public class EnfermeiroClass
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string nome { get; set; }
+
+        public string Bloco { get; set; }
+        [Required]//1->60 anos;2-filhos menores;3-outros
+        public int tipo { get; set; }
+
+        
+
+    }
+}
