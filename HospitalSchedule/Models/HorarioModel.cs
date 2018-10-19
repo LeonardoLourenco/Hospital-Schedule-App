fcd6832eaff70010e083e2d84e22a937a3604a6e
Supt <1012388@sal.ipg.pt>
2018-10-19 10:31:59 +0100

    Criação do Controlador,do Modelo e da view para o Horário

---
+++ HospitalSchedule/Models/HorarioModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace HospitalSchedule.Models
+{
+    public class HorarioModel
+    {
+        [Required]
+        public int HorarioId{ set ; get; }
+
+        [Required]
+        public List<String> Dias_Semana { set; get; }
+
+        [Required]
+        public List <int> Horas { set; get; }
+
+        [Required]
+        public int Turno { set; get; }
+
+        [Required]
+        public String NomeEnfermeiro { set; get; }
+
+        [Required]
+        public DateTime Data { set; get; }
+
+        [Required]
+        public String Bloco { set; get; }
+    }
+}
