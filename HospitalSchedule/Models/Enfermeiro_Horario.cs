c378198932ee9b18dd61c1f910f6c490656c272e
Supt <1012388@sal.ipg.pt>
2018-11-02 12:57:59 +0000

    Criação dos modelos de acordo com o modelo ER do projeto

---
+++ HospitalSchedule/Models/Enfermeiro_Horario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalSchedule.Models
+{
+    public class Enfermeiro_Horario
+    {
+        [Required]
+        public int Enfermeiro_horarioID { get; set; }//Chave primária(Guarda os horários,
+                                                        //principal e o de prevenção)
+        [Required]
+        public Schedule ScheduleID {get;set; }//Chave estrangueira do horário
+
+        [Required]
+        public Enfermeiro EnfermeiroID { get; set; }//Chave estrangueira do enfermeiro
+    }
+}
