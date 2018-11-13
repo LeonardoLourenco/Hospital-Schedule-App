4f167ab378758f329660b84ee6a84a525f824799
LucianoBrido <lucianofernandes@live.com.pt>
2018-11-13 14:04:12 +0000

    CRUDNurse

---
+++ HospitalSchedule/Controllers/NurseController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HospitalSchedule.Models;
+
+namespace HospitalSchedule.Controllers
+{
+    public class NurseController : Controller
+    {
+        private readonly HospitalScheduleDbContext _context;
+
+        public NurseController(HospitalScheduleDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Nurse
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Nurse.ToListAsync());
+        }
+
+        // GET: Nurse/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nurse = await _context.Nurse
+                .FirstOrDefaultAsync(m => m.NurseID == id);
+            if (nurse == null)
+            {
+                return NotFound();
+            }
+
+            return View(nurse);
+        }
+
+        // GET: Nurse/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Nurse/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NurseID,Name,Type,Specialties,Email,CellPhoneNumber")] Nurse nurse)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nurse);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nurse);
+        }
+
+        // GET: Nurse/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nurse = await _context.Nurse.FindAsync(id);
+            if (nurse == null)
+            {
+                return NotFound();
+            }
+            return View(nurse);
+        }
+
+        // POST: Nurse/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("NurseID,Name,Type,Specialties,Email,CellPhoneNumber")] Nurse nurse)
+        {
+            if (id != nurse.NurseID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nurse);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NurseExists(nurse.NurseID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(nurse);
+        }
+
+        // GET: Nurse/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var nurse = await _context.Nurse
+                .FirstOrDefaultAsync(m => m.NurseID == id);
+            if (nurse == null)
+            {
+                return NotFound();
+            }
+
+            return View(nurse);
+        }
+
+        // POST: Nurse/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var nurse = await _context.Nurse.FindAsync(id);
+            _context.Nurse.Remove(nurse);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NurseExists(int id)
+        {
+            return _context.Nurse.Any(e => e.NurseID == id);
+        }
+    }
+}
