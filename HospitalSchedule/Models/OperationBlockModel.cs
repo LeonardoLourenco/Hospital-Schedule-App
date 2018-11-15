ab88390795be1583cb1ac18b1eb03997ce6cc0af
LeonardoLourenço <leonardo-lourenco@hotmail.com>
2018-11-15 17:15:48 +0000

    Scaffolded 2 models (Operation Block and Nurse_Schedule)

--- HospitalSchedule/Models/OperationBlockModel.cs
+++ HospitalSchedule/Models/OperationBlockModel.cs
@@ -13,17 +13,11 @@ namespace HospitalSchedule.Models
         public int OperationBlockID { get; set; }            //Block Id
 
         [Required]
-        [RegularExpression(@"[A-Z]+(_Reserva)?", ErrorMessage = "Invalid Block Name.")] //[A-Z]+(_Reserva)? Significa letra maiuscula de A a Z
-        public string BlockName { get; set; }       //Nome do bloco                     // com _Reserva presente ou não ex: A e A_Reserva
+        public string BlockName { get; set; }       //Nome do bloco       também é aqui que é indicado se é de prevenção ou não             
 
         [Required]
-        [RegularExpression(@"[0-9]", ErrorMessage = "Invalid Max Number Of Nurses.")]
-        public int MaxNumOfNurses { get; set; }     //Numero máximo de enfermeiros
-
-        [Required]
-        [RegularExpression(@"[0-9]", ErrorMessage = "Invalid Current Nurses number.")]
-        public int CurrentNurses { get; set; }      //Numero actual de enfermeiros
-
+        public string TypeOfShift { get; set; }     //Os tipos de turnos presentes neste bloco M - manha T - Tarde N - Noite
+                                                    //Ex M:T:N funciona de manha,tarde e noite                
 
         //chave estrangueira do horário
         public Schedule Schedule { get;set; }
