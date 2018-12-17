6d33848506fc0d2c589738bc585240f44fff6bb0
LucianoBrido <lucianofernandes@live.com.pt>
2018-12-17 21:18:47 +0000

    Validation Speciality for DB

--- HospitalSchedule/Controllers/SpecialtiesController.cs
+++ HospitalSchedule/Controllers/SpecialtiesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using HospitalSchedule.Models;
+using System.Text.RegularExpressions;
 
 namespace HospitalSchedule.Controllers
 {
@@ -52,6 +53,9 @@ namespace HospitalSchedule.Controllers
         public async Task<IActionResult> Index(string search, int page = 1)
         {
             int numSpecialty = await _context.Specialty.CountAsync();
+            
+
+
 
             //se nao tiver nada na pesquisa retorna a view anterior
             if (String.IsNullOrEmpty(search))
@@ -115,6 +119,22 @@ namespace HospitalSchedule.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("SpecialtyId,Name")] Specialty specialty)
         {
+            var name = specialty.Name;
+
+            //validaçoes de email na DataBase
+            if (!nameIsValid(name))
+            {
+                ModelState.AddModelError("Name", "Speciality is invalid");
+            }
+
+
+            if (nameIsInvalid(name) == true)
+            {
+                ModelState.AddModelError("Name", "Specality already exist");
+            }
+
+
+
             if (ModelState.IsValid)
             {
                 _context.Add(specialty);
@@ -208,6 +228,44 @@ namespace HospitalSchedule.Controllers
         private bool SpecialtyExists(int id)
         {
             return _context.Specialty.Any(e => e.SpecialtyId == id);
+        }
+
+
+        private bool nameIsInvalid(string name)
+        {
+            bool IsInvalid = false;
+            //Procura na BD se existem enfermeiros com o mesmo email
+            var specialities = from e in _context.Specialty
+                         where e.Name.Contains(name)
+                         select e;
+
+            if (!specialities.Count().Equals(0))
+            {
+                IsInvalid = true;
+            }
+            return IsInvalid;
+        }
+
+
+        public static bool nameIsValid(string name)
+        {
+            string expression;
+            expression = "[a-zA-Z][a-zA-Z ]*";
+            if (Regex.IsMatch(name, expression))
+            {
+                if (Regex.Replace(name, expression, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
